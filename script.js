9c5f9889c02ba968a137de3c8147db3583a75239
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-10-20 11:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
-  let b=Number.toBinaryString(num);
-  return parseInt(quantity,10);
+  if(num==0) return 0;
+  let ans="";
+  while(num>0){
+	 ans=(num%2)+ans;
+	 num=Math.floor(num/2);
+  }
+	return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
